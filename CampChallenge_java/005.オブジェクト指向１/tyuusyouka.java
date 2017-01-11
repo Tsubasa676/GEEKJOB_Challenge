bb51b72e698a494625cab9616145433d742552a4
Tsubasa <stst5374xS>
2017-01-11 09:58:22 +0900

    オブジェクト指向、課題１・２

---
+++ CampChallenge_java/005.オブジェクト指向１/tyuusyouka.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mbp13
+ */
+public class tyuusyouka {
+   
+//   ＜抽象化１＞
+//　　・キャンプ生　　 役目：キャンプ生
+//   　　　　　　　　　情報：名前、年齢、言語、選択コース、経験年数
+//        　　　　　　動作：学習する、報告連絡相談する
+//   ・イラストレーター　役目：イラストレーター
+//   　　　　　　　　　情報：ペンネーム、年齢、ジャンル（画風）、経験年数
+//        　　　　　　動作：作品を作る、宣伝する
+//   ・USBメモリ　　役目：データ保管
+//           　　　情報：規格、読み書き速度、容量
+//                動作：書き込み、書き出し
+//
+//   ＜抽象化２＞
+//   ・キャンプ生、イラストレーター
+//　　　　　　　　人→名前（ペンネーム）、年齢、経験年数
+//　　・USBメモリ
+//　　　　　　　　物→容量、規格、読み書き速度   
+   
+}
