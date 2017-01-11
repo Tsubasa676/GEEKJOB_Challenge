a47d45c9071fdbb14579c4c0f2d00807370c6b51
Tsubasa <stst5374xS>
2017-01-11 10:16:59 +0900

    クラス作成

---
+++ CampChallenge_java/005.オブジェクト指向１/ClassKadai12.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mbp13
+ */
+public class ClassKadai12 {
+   //課題１　クラス作成
+   //パブリックな変数２つ用意
+   public String  a = "";
+   public Integer b = 0;
+   //パブリックなメソッド２つ
+   public void setClassKadai12(String c,Integer d){
+      
+   }
+   //２つの変数の中身をprint
+   public void showA(){
+      
+   }
+   public void showB(){
+      
+   }
+
+
+}
