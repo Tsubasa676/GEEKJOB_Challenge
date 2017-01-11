f3b1418e22b3df31dc15e309e790464362e8f2fc
Tsubasa <stst5374xS>
2017-01-11 18:16:48 +0900

    オブジェクト指向１課題３

--- CampChallenge_java/005.オブジェクト指向１/ClassKadai12.java
+++ CampChallenge_java/005.オブジェクト指向１/ClassKadai12.java
@@ -1,29 +1,40 @@
+package org.ClassKadai;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.io.PrintWriter;
 /**
  *
  * @author mbp13
  */
 public class ClassKadai12 {
-   //課題１　クラス作成
-   //パブリックな変数２つ用意
-   public String  a = "";
-   public Integer b = 0;
-   //パブリックなメソッド２つ
-   public void setClassKadai12(String c,Integer d){
+//課題１　「クラス作成」ここから
+   //パブリックな変数２つ用意(フィールド)
+   public int a;
+   public int b;
+   //２つの変数に値を設定するパブリックなメソッド
+   public void A(){
+      a=12;
+   }
+   public void B(){
+      b=24;
+   }
+   //２つの変数の中身をprintするパブリックなメソッド
+   public void C(PrintWriter out,int a,int b){
+      //サーブレット「System.outとoutは異なる」
+      out.print(a);
+      out.print(b);
+   }
+//課題１　「クラス作成」ここまで
+//ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+//課題２　「クラス継承」ここから
+   //２つの変数の中身をクリアするパブリックなメソッド
+   public class ClaKei extends ClassKadai12{
       
    }
-   //２つの変数の中身をprint
-   public void showA(){
-      
-   }
-   public void showB(){
-      
-   }
-
+//課題２　「クラス継承」ここまで
 
 }
